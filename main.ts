644c4153d9c626327f154f4ef6109ab964ee086a
rinkexe <rinkexe@icloud.com>
2026-03-11 11:14:43 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     characterAnimations.setCharacterState(playerSprite, characterAnimations.rule(Predicate.MovingUp))
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(assets.image`projectile`, playerSprite, 50, 0)
+    pause(1000)
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     characterAnimations.setCharacterState(playerSprite, characterAnimations.rule(Predicate.MovingLeft))
 })
@@ -382,6 +386,7 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     characterAnimations.setCharacterState(playerSprite, characterAnimations.rule(Predicate.MovingDown))
 })
+let projectile: Sprite = null
 let playerSprite: Sprite = null
 scene.setBackgroundColor(7)
 playerSprite = sprites.create(img`
